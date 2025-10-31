c65ffb03deabce30095d6827121bc786d5466287
alihojatnia <77141595+alihojatnia@users.noreply.github.com>
2025-10-31 18:44:57 +0330

    Fix method name in test_doe function

--- tests/test_doe.py
+++ tests/test_doe.py
@@ -7,7 +7,7 @@ from src.doe_generator import generate_doe_smart
 
 def test_doe():
     factors = {"pH": [6.0, 8.0], "NaCl_mM": [0, 500]}
-    df = generate_doe_smart(factors, method="full_fact")
+    df = generate_doe_smart(factors, method="fullfact")
     assert isinstance(df, pd.DataFrame)
     assert len(df) == 4                     # 2^2 = 4 runs
     assert "Run" in df.columns
